e5bbc2194fbac14be63a7b9c8f327294dee7ae9e
Yjrjoy <160854170+Yjrjoy@users.noreply.github.com>
2024-02-22 20:43:40 +0600

    Add files via upload

---
+++ 2231081065_problem3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Transactions;
+namespace problem 
+{
+
+    class Hello 
+    {
+
+        static void Main(string[] args)
+        {
+           Console.Write("Enter the first number of the series : ");
+           int n = Convert.ToInt32(Console.ReadLine());
+           for(int i = n;i >= 0; i--)
+           {
+                Console.Write(i + " ");
+                if (i != 0)
+                {
+                Console.Write(" ");
+                }
+           }
+        }
+    }
+}
+
